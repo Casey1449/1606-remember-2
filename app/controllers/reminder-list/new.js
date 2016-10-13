42aa612049a597eceb73edc08a9ba39a4be9c7bb
Casey Metz <caseymetz@gmail.com>
2016-10-13 16:22:45 -0600

    convert new-reminder component to controller

---
+++ app/controllers/reminder-list/new.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+
+    title: '',
+    date: '',
+    notes: '',
+
+    actions: {
+      createReminder() {
+        const reminder = this.getProperties('title', 'date', 'notes');
+        this.get('store').createRecord('reminder', reminder).save().then(() => {
+          this.setProperties({ title: '', date: '', notes: '' });
+        });
+      }
+    }
+});
