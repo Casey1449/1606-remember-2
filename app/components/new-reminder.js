dd160c440a39fa6aa63782f2217a0472908687c7
Casey Metz <caseymetz@gmail.com>
2016-10-13 15:30:38 -0600

    debug date format

--- app/components/new-reminder.js
+++ app/components/new-reminder.js
@@ -4,17 +4,16 @@ export default Ember.Component.extend({
   store: Ember.inject.service(),
 
   tagName: 'section',
-  
+
   title: '',
   date: '',
   notes: '',
 
   actions: {
     createReminder() {
-      console.log('create function fired');
       const reminder = this.getProperties('title', 'date', 'notes');
       this.get('store').createRecord('reminder', reminder).save().then(() => {
-        this.setProperties({ title: '', date: '', notes: '' });
+        this.setProperties({ title: '', date: 'sdfsdf', notes: '' });
       });
     }
   }
