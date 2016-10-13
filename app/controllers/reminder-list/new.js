266f16d3fde04ff465b797a9e1df165d37c14a56
Casey Metz <caseymetz@gmail.com>
2016-10-13 17:50:24 -0600

    add passing Acceptance tests for reminder input form

--- app/controllers/reminder-list/new.js
+++ app/controllers/reminder-list/new.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-
     title: '',
     date: '',
     notes: '',
---
+++ tests/acceptance/reminder-list/new-test.js
@@ -0,0 +1,33 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'remember/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | reminder list/new', {
+  beforeEach(){
+    server.createList('reminder', 5);
+  },
+  afterEach(){
+    server.shutdown();
+  }
+});
+
+test('visiting new', function(assert) {
+  visit('/new');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/new');
+  });
+});
+
+test('viewing the homepage', function(assert) {
+
+  visit('/new');
+  fillIn('.reminder-title-input', 'Some title');
+  fillIn('.reminder-notes-input', 'Some notes');
+  click('.reminder-submit-button');
+
+  andThen(function() {
+    assert.equal(Ember.$('.spec-reminder-item').length, 6);
+    assert.equal(Ember.$('.spec-reminder-item:last').text(), 'Some title');
+    assert.equal(Ember.$('.reminder-item-notes:last').text(), 'Some notes');
+  });
+});
