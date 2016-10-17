90589f23c3b505ba75e93fd0fdc0319b6379e171
Casey Metz <caseymetz@gmail.com>
2016-10-17 17:14:35 -0600

    commiting to switch

--- app/controllers/reminder-list/edit.js
+++ app/controllers/reminder-list/edit.js
@@ -2,15 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
+
       updateReminder(id) {
         this.get('store').findRecord('reminder', id).then((reminder) => {
           reminder.save().then((reminder) => { this.transitionToRoute('reminder-list.reminder-detail', reminder.id); });
         });
       },
+
       revertReminder(id) {
         this.get('store').findRecord('reminder', id).then((reminder) => {
           reminder.rollbackAttributes();
         });
       }
+
     }
 });
