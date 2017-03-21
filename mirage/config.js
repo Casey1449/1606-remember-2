dd8f90af376811fe98079c156d258a590840d989
Casey Metz <caseymetz@gmail.com>
2017-03-21 16:36:45 -0600

    initial gh-pages commit

--- mirage/config.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  this.get('/reminders');
-  this.post('/reminders');
-  this.get('/reminders/:id');
-  this.patch('/reminders/:id');
-  this.put('/reminders/:id');
-  this.del('/reminders/:id');
-}
